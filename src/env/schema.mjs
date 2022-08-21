4c026ad8ce3baae270aaf164907347e80a60a95e
yxshv <yashverma30115@gmail.com>
2022-08-21 16:51:36 +0530

    add `REDIS_CONNECTION_STRING`

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -13,7 +13,8 @@ export const serverSchema = z.object({
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
   DISCORD_TOKEN: z.string(),
-  DISCORD_IMAGES_CHANNEL_ID: z.string()
+  DISCORD_IMAGES_CHANNEL_ID: z.string(),
+  REDIS_CONNECTION_STRING: z.string()
 });
 
 /**
