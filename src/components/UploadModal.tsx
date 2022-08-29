a9289f868f85964ca2c6b9239bfcb9938203429a
yxshv <yashverma30115@gmail.com>
2022-08-29 08:42:00 +0530

    fix theme

--- src/components/UploadModal.tsx
+++ src/components/UploadModal.tsx
@@ -48,12 +48,12 @@ function UploadModal({ input }: { input: File }) {
     <div>
       {/* Image preview in small box */}
       <div className="flex flex-col items-center justify-center overflow-y-auto">
-        <div className="md:absolute md:top-10 w-44 bg-dark rounded-md flex flex-col items-center p-3">
+        <div className="md:absolute md:top-12 max-h-80 bg-dark rounded-md flex flex-col items-center p-3">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src={URL.createObjectURL(input)}
             alt="Upload preview"
-            className="object-cover w-40 h-40"
+            className=""
           />
           <span className="mt-2 font-mono">{input.name}</span>
           <span className="text-xs text-gray-500">
@@ -62,13 +62,13 @@ function UploadModal({ input }: { input: File }) {
         </div>
 
         {/* Inputs */}
-        <div className="flex flex-col  md:mx-10 gap-4 mt-10 md:mt-52 ">
-          <div>
-            <label htmlFor="nameInput" className="block text-sm text-gray-400">
+        <div className="flex w-10 justify-center items-center flex-col md:mx-10 gap-4 mt-10 md:mt-52 ">
+          <div className="">
+            <label htmlFor="nameInput" className="block text-sm -mb-1 text-gray-400">
               Slug
             </label>
             <input
-              className="text-white bg-dark rounded-md px-2 py-1 mt-2"
+              className="text-rose-400 bg-rose-400/10 rounded-md px-4 py-2 mt-2"
               defaultValue="Auto"
               id="nameInput"
               type="text"
@@ -78,12 +78,12 @@ function UploadModal({ input }: { input: File }) {
           </div>
 
           {/* Upload button */}
-          <div className="flex items-center ">
+          <div className="flex w-full items-center justify-center">
             <button
               onClick={async () => {
                 uploadImg(input, name);
               }}
-              className="px-3 py-1 bg-blue-500 rounded-md"
+              className="px-4 text-sm py-2 text-rose-400 hover:text-rose-300 hover:bg-rose-400/50 bg-rose-400/30 rounded-md"
             >
               Upload
             </button>
